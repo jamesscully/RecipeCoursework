d116171fd217faaf81f81da3c5f2cc2d916ceb20
James Scully <james.will.scully@gmail.com>
2019-11-26 23:28:11 +0000

    Reviewing recipe seems to be all good

--- app/src/main/java/com/scullyapps/recipebook/OpenRecipe.kt
+++ app/src/main/java/com/scullyapps/recipebook/OpenRecipe.kt
@@ -2,6 +2,8 @@ package com.scullyapps.recipebook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.scullyapps.recipebook.widgets.IngredientView
+import kotlinx.android.synthetic.main.activity_open_recipe.*
 
 // this class is used to rate, delete or edit an activity.
 
@@ -12,5 +14,23 @@ class OpenRecipe : AppCompatActivity() {
         setContentView(R.layout.activity_open_recipe)
 
         supportActionBar?.hide()
+
+
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+        layout_ingredients.addView(IngredientView(this, "This is an ingredient", "1337g"))
+
     }
 }
